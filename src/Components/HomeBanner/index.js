3b3728a5df69d920c74f333e56795f9eed998d75
tandukaruniqur <uniquetandukar143@gmail.com>
2024-07-15 20:48:13 +0545

    Update

--- src/Components/HomeBanner/index.js
+++ src/Components/HomeBanner/index.js
@@ -8,34 +8,33 @@ const HomeBanner = () => {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    // arrows:true,
+    arrows:true,
   };
 
   return (
     <div className="homeBannerSection">
       <Slider {...settings}>
-        <div className="item" >
-            
+        <div className="item">
           <img
-            src="https://sslimages.shoppersstop.com/sys-master/root/h8e/hb2/33109669904414/Main_EOSS-FLAT-50-WEB--2024-07--10-new-hp.jpg"
+            src="https://sslimages.shoppersstop.com/sys-master/root/h8e/hb2/33109669904414/Main_EOSS-FLAT-50-WEB--2024-07--10-new-hp.jpg" 
             alt="Banner 1"
           />
         </div>
         <div className="item">
           <img
-            src="https://sslimages.shoppersstop.com/sys-master/root/h26/h44/32916137639966/Bullmer-web_48-jw8.jpg"
+            src="https://sslimages.shoppersstop.com/sys-master/root/h26/h44/32916137639966/Bullmer-web_48-jw8.jpg" 
             alt="Banner 2"
           />
         </div>
         <div className="item">
           <img
-            src="https://sslimages.shoppersstop.com/sys-master/root/hdc/h2f/32916137050142/selected-web_38-dk.jpg"
+            src="https://sslimages.shoppersstop.com/sys-master/root/hdc/h2f/32916137050142/selected-web_38-dk.jpg" className="w-100"
             alt="Banner 3"
           />
         </div>
         <div className="item">
           <img
-            src="https://sslimages.shoppersstop.com/sys-master/root/hf4/h4c/33109699657758/Timex-web_9--ka9.jpg"
+            src="https://sslimages.shoppersstop.com/sys-master/root/hf4/h4c/33109699657758/Timex-web_9--ka9.jpg" className="w-100"
             alt="Banner 4"
           />
         </div>
