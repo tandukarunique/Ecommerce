ab3766520aad8f3c44ffddd9dd67ffabf6392f9e
tandukaruniqur <uniquetandukar143@gmail.com>
2024-08-04 08:33:38 +0545

    Viewall

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,7 +1,18 @@
 import HomeBanner from "../../Components/HomeBanner";
-import banner1 from "../../assests/images/Banner1.png"
+import { Button } from "@mui/material";
+import banner1 from "../../assests/images/Banner1.png";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import React from "react";
+import Slider from "react-slick";
 
 const Home = () => {
+  var productsSliderOptions = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
   return (
     <>
       <HomeBanner />
@@ -10,18 +21,28 @@ const Home = () => {
           <div className="row">
             <div className="col-md-3">
               <div className="banner">
-              <img
-                src= {banner1} alt="banner1"
-                className="cursor w-100" 
-              />
+                <img src={banner1} alt="banner1" className="cursor w-100" />
               </div>
             </div>
 
-            <div className="col-ml-2 productRow" ></div>
+            <div className="col-ml-2 productRow"></div>
             <div className="d-flex align-items-left">
               <div className="info w-100">
                 <h3 className="ml-0 hd">BEST SELLERS</h3>
-                <p className="text-light text-sml mb-0 "> Do not miss the current offers </p>
+                <p className="text-light text-sml mb-0 ">
+                  {" "}
+                  Do not miss the current offers{" "}
+                </p>
+              </div>
+              <div className="button-container" style={{ marginLeft: "auto" }}>
+                <Button className="viewAllBtn">
+                  View all <IoIosArrowRoundForward />
+                </Button>
+              </div>
+              <div className="product_row">
+                <Slider {...productsSliderOptions}>
+                  <div className="item"></div>
+                </Slider>
               </div>
             </div>
           </div>
