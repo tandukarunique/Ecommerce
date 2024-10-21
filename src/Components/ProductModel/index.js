fc77f70fa201ada897c68c40b5a4b6d7b40350ba
tandukaruniqur <uniquetandukar143@gmail.com>
2024-10-21 21:39:46 +0545

    Detail option

--- src/Components/ProductModel/index.js
+++ src/Components/ProductModel/index.js
@@ -1,4 +1,21 @@
-const ProductModel = () => {
-  return <></>;
+import Dialog from "@mui/material/Dialog";
+import { IoIosClose } from "react-icons/io";
+import { Button } from "@mui/material";
+
+const ProductModel = (props) => {
+  return (
+    <>
+      <Dialog
+        open={true}
+        className="productModel"
+        onClose={() => props.CloseProductModel(false)}
+      >
+        <Button className="close_">
+          <IoIosClose />
+        </Button>
+        <h4 class="mb-0"> All Natural </h4>
+      </Dialog>
+    </>
+  );
 };
 export default ProductModel;
