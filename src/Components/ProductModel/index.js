75fc8cb824f93e74edbcb28073a1ef4abc8c8228
tandukaruniqur <uniquetandukar143@gmail.com>
2024-10-24 00:07:44 +0545

    Productmodeldetail

--- src/Components/ProductModel/index.js
+++ src/Components/ProductModel/index.js
@@ -1,6 +1,7 @@
 import Dialog from "@mui/material/Dialog";
 import { IoIosClose } from "react-icons/io";
 import { Button } from "@mui/material";
+import Rating from '@mui/material/Rating';
 
 const ProductModel = (props) => {
   return (
@@ -8,12 +9,24 @@ const ProductModel = (props) => {
       <Dialog
         open={true}
         className="productModel"
-        onClose={() => props.CloseProductModel(false)}
+        onClose={() => props.CloseProductModel()}
       >
-        <Button className="close_">
+        <Button className="close_ d-flex" style={{display:"flex",fontSize:"32px",marginLeft:"auto"}} onClick={() => props.CloseProductModel()}>
           <IoIosClose />
         </Button>
-        <h4 class="mb-0"> All Natural </h4>
+        <h4 class="mb-3 "  > All Natural ingrediants used in the because is then what</h4>
+        <div className="d-flex align-items">
+          <div className="d-flex align-items-centre mr-4 ml-2">
+            {" "}
+            <span >Brands: </span>
+            <span className="ml-2">welch's</span>
+          </div>
+          <Rating name="read-only" value={5} size="small" precision={0.5}readOnly />
+          <div className="row mt-2">
+            <div className="col-md-5"></div>
+            <div className="col-md-7"></div>
+          </div>
+        </div>
       </Dialog>
     </>
   );
